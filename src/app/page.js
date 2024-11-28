3fa4d8d6f41d974b062cfd31cfc7fa55a83c7679
seou <idh02379@naver.com>
2024-11-28 17:27:58 +0900

    페이지 구성 / 이미지 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <section>
-        <div>
+        <div className="container">
         <div className="text-[15vw] font-black">thinK Great</div>
         <div className="relative">
-        <Image src="/images/intro.jpg" alt=""
+        <Image src="/images/midea01.jpg" alt=""
          width={1300} height={1300} className="w-full h-[400px]
          object-cover" />
         <div className="absolute left-5 bottom-10 flex flex-col gap-2">
@@ -16,6 +17,26 @@ export default function Home() {
         <strong className="text-white text-3xl font-bold">KB그룹광고 <br /> 드디어 TV방송에서 <br /> 만나다.</strong>
         </div>
         </div>
+        </div>
+      </section>
+
+
+      <section>
+        <div className="container">
+          <ul>
+            <li>
+              <Link href="#" className="flex flex-col gap-3">
+              <Image src="/images/midea02.jpg" alt="" width={1300} height={1300} 
+              className="w-full aspect-video object-cover" />
+              <div className="flex flex-col">
+              <span className="text-md text-gray-500">소셜미디어</span>
+              <strong className="text-xl">소통과 화합의 현장!<br />
+               KG모빌리티 파트너스 데이</strong>
+              </div>
+
+              </Link>
+            </li>
+          </ul>
         </div>
       </section>
     </div>
